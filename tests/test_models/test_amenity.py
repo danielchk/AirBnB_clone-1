7101122df47fe5f758e4388bbd82c397ca78cc92
danielchk <hollowdaniel1@gmail.com>
2020-12-15 09:35:28 -0500

    adding test in amenity --ok

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-""" """
+"""
+
+"""
+import models
+import unittest
+import MySQLdb
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
@@ -17,3 +22,23 @@ class test_Amenity(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_subclass(self):
+        """amenity is a subclass"""
+        inst = Amenity()
+        self.assertIsInstance(inst, BaseModel)
+        self.assertTrue(hasattr(inst, "id"))
+        self.assertTrue(hasattr(inst, "created_at"))
+        self.assertTrue(hasattr(inst, "updated_at"))
+
+    def test_output(self):
+        """show if the output works"""
+        inst = Amenity()
+        out = "[Amenity] ({}) {}".format(inst.id, inst.__dict__)
+        self.assertEqual(out, str(inst))
+
+    def test_name(self):
+        """test name exists in amenity instance"""
+        inst = Amenity()
+        self.assertTrue(hasattr(inst, "name"))
+        self.assertEqual(inst.name, "")
