5693d6855743a0a5fedf1597708bc20cbbdb0787
danielchk <hollowdaniel1@gmail.com>
2020-12-15 15:41:37 -0500

    some tests to the console

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,3 +10,29 @@ from io import StringIO
 from console import HBNBCommand
 from unittest.mock import create_autospec
 
+class test_console(unittest.TestCase):
+    """
+    test for the console
+    """"
+
+    def test_quit(self):
+        ''' 
+        test quit
+        '''
+        console = self.create()
+        self.assertTrue(console.onecmd("quit"))
+
+    def test_EOF(self):
+        """
+        test EOF
+        """
+        console = self.create()
+        self.assertTrue(console.onecmd("EOF"))
+
+    def test_all(self):
+        """
+        test all
+        """
+        console = self.create()
+        console.onecmd("all")
+        self.assertTrue(isinstace(self.capt_out.getvalue(), str))
