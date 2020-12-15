556d8bf24ed599ca997089cbe2eba9a6cc4f1c89
danielchk <hollowdaniel1@gmail.com>
2020-12-15 15:15:13 -0500

    more tests

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' 
+Test suite for the console
+'''
+
+import models
+import sys
+import unittest
+from io import StringIO
+from console import HBNBCommand
+from unittest.mock import create_autospec
+
