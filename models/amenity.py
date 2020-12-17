55c71b998c57ac046b645ec5373a6243c466c691
danielchk <hollowdaniel1@gmail.com>
2020-12-17 16:07:26 -0500

    error

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel):
+    name = ""
--- models/city.py
+++ models/city.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationshi
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    state_id = ""
+    name = ""
