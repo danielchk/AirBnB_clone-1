c568e6e588596efdd37c28daeb26d9201fbb2d99
danielchk <hollowdaniel1@gmail.com>
2021-01-20 14:24:39 -0500

    trying 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""initializate flask"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """show Hello HBNB!"""
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def functionc(text):
+    """show C followed by text replace to _ or space"""
+    return ('C ' + text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def functionp(text='is cool'):
+    """show Python followed by text replace to or space"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """if int show: "n" is a number"""
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbertemplates(n):
+    """if number show template(HTML)"""
+    return render_template('5-number.html', n=n)
+
+
+def oddoreven(n):
+    """display a HTML page only if n is an integer"""
+    if n % 2 == 0:
+        dig = 'even'
+    else:
+        dig = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n,
+                           dig=dig)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
