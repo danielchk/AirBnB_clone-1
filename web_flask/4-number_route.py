163e6d93e3517e88ccaa83cf7fad56acb38e393e
danielchk <hollowdaniel1@gmail.com>
2021-01-20 13:56:08 -0500

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,28 +6,34 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-	"""show Hello HBNB!"""
-	return ('Hello HBNB!')
+    """show Hello HBNB!"""
+    return ('Hello HBNB!')
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""returns HBNB"""
-	return ('HBNB')
+    """returns HBNB"""
+    return ('HBNB')
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def functionc(text):
-	"""show C followed by text replace to _ or space"""
-	return ('C ' + text.replace('_', ' '))
+    """show C followed by text replace to _ or space"""
+    return ('C ' + text.replace('_', ' '))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def functionp(text='is cool'): 
-	"""show Python followed by text replace to or space"""
-	return 'Python ' + text.replace('_',' ')
+def functionp(text='is cool'):
+    """show Python followed by text replace to or space"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """if int show: "n" is a number"""
+    return "{:d} is a number".format(n)
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
